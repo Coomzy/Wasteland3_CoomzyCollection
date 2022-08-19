bbb1ca5b62d94fca7fa92d93e54c97f80a605927
Coomzy <ccoomzy@hotmail.co.uk>
2022-08-19 23:26:31 +0100

    Init Commit

---
+++ FreeRetrain/FreeRetrain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+//using Harmony; // If you're using 0.3.0 and lower
+using HarmonyLib;
+using InXile.Trade;
+using InXile.UI.WL3;
+
+[assembly: MelonInfo(typeof(FreeRetrain.FreeRetrain), "Free Retrain", "1.0.0", "Coomzy")]
+[assembly: MelonGame("InxileEntertainment", "Wasteland 3")]
+namespace FreeRetrain
+{
+    public class FreeRetrain : MelonMod { }
+
+    [HarmonyPatch(typeof(RetrainCharacterUtil), "GetCostForRetrain", MethodType.Normal)]
+    class Patch_1 { static void Postfix(ref int __result) { __result = 0; } }
+
+    [HarmonyPatch(typeof(RetrainCharacterUtil), "CanAffordToRetrain", MethodType.Normal)]
+    class Patch_2 { static void Postfix(ref bool __result) { __result = true; } }
+}
---
+++ PartyOfSix/PartyOfSix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+//using Harmony; // If you're using 0.3.0 and lower
+using HarmonyLib;
+using InXile.Trade;
+using InXile.UI.WL3;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(PartyOfSix.PartyOfSix), "Party of Six", "1.0.0", "Coomzy")]
+[assembly: MelonGame("InxileEntertainment", "Wasteland 3")]
+namespace PartyOfSix
+{
+    public class PartyOfSix : MelonMod {}
+
+    [HarmonyPatch(typeof(PartyMemberTrade), "GetNumPlayerCreatedCharactersAfterTrade", MethodType.Normal)]
+    class Patch_1 { static void Postfix(ref int __result) { __result = 3; } }
+
+    [HarmonyPatch(typeof(PartyMemberTrade), "WillPartyBeOverfilled", MethodType.Normal)]
+    class Patch_2 { static void Postfix(ref bool __result) { __result = false; } }
+
+    [HarmonyPatch(typeof(PartyMemberTrade), "WillPartiesHaveTooManyCNPCs", MethodType.Normal)]
+    class Patch_3 { static void Postfix(ref bool __result) { __result = false; } }
+
+    [HarmonyPatch(typeof(PartyMemberTrade), "WillPartiesHaveLessThanMaxCreatedCharacters", MethodType.Normal)]
+    class Patch_4 { static void Postfix(ref bool __result) { __result = true; } }
+
+    [HarmonyPatch(typeof(PartyManagementWindow), "DoesSinglePlayerHaveLessThanMaxCreatedCharacters", MethodType.Normal)]
+    class Patch_5 { static void Postfix(ref bool __result) { __result = true; } }
+
+    [HarmonyPatch(typeof(PartyManagementWindow), "DoBothPartiesHaveLessThanMaxCreatedCharacters", MethodType.Normal)]
+    class Patch_6 { static void Postfix(ref bool __result) { __result = true; } }
+}
