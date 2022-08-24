6111e8c9fd8f5fc04f782c8cffb72b01e848128c
Coomzy <ccoomzy@hotmail.co.uk>
2022-08-24 03:18:14 +0100

    Added World Map Fast Travel

---
+++ Test/Test.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+//using Harmony; // If you're using 0.3.0 and lower
+using HarmonyLib;
+using InXile.Trade;
+using InXile.UI.WL3;
+using UnityEngine;
+using InXile.Scenes;
+
+[assembly: MelonInfo(typeof(Test.Test), "Test", "1.0.0", "Tester")]
+[assembly: MelonGame("InxileEntertainment", "Wasteland 3")]
+namespace Test
+{
+    public class Test : MelonMod
+    {
+        public static Test instance;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+
+            instance = this;
+
+            Log($"Test Init!");
+        }
+
+        void LogInternal(string log)
+        {
+            LoggerInstance.Msg(log);
+        }
+
+        public static void Log(string log)
+        {
+            instance.LogInternal(log);
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldMapManager), "SetPlayerDestination_Server", MethodType.Normal)]
+    class Patch_1
+    {
+        static void Postfix(ref Vector3 destination, ref bool showPath, ref WorldMapManager __instance)
+        {
+            Test.Log($"SetPlayerDestination_Server() destination = {destination}, showPath = {showPath}");
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldMapManager), "StartAutoTravel_Server", MethodType.Normal)]
+    class Patch_2
+    {
+        static void Postfix(ref Vector3 destination, ref WorldMapManager __instance)
+        {
+            Test.Log($"StartAutoTravel_Server() destination = {destination}");
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldMapManager), "SetWorldMapPromptActive", MethodType.Normal)]
+    class Patch_3
+    {
+        static void Postfix(ref bool newState, ref WorldMapManager __instance)
+        {
+            Test.Log($"SetWorldMapPromptActive() newState = {newState}");
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldMapManager), "RequestTransition", MethodType.Normal)]
+    class Patch_5
+    {
+        static void Postfix(ref WorldMapSceneStartOption startOption, ref WorldMapManager __instance)
+        {
+            Test.Log($"RequestTransition() startOption = {startOption}");
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldMapManager), "OpenPrompt", MethodType.Normal)]
+    class Patch_6
+    {
+        static void Postfix(ref WorldMapTransition transition, ref WorldMapManager __instance)
+        {
+            Test.Log($"OpenPrompt() transition = {transition.returnLocationIdentifier}");
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldMapManager), "DoTransitionMap", MethodType.Normal)]
+    class Patch_7
+    {
+        static void Postfix(ref Scenes scene, ref WorldMapManager __instance)
+        {
+            Test.Log($"DoTransitionMap() scene = {scene}");
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldMapManager), "TransitionMap", MethodType.Normal)]
+    class Patch_8
+    {
+        static void Postfix(ref Scenes sceneToLoad, ref WorldMapManager __instance)
+        {
+            Test.Log($"TransitionMap() sceneToLoad = {sceneToLoad}");
+        }
+    }
+
+    [HarmonyPatch(typeof(Cutscene_0_TowToHQ), "StartCutsceneImpl", MethodType.Normal)]
+    class Patch_9
+    {
+        static void Postfix(ref Cutscene_0_TowToHQ __instance)
+        {
+            if (__instance?.WP_TeleportDestination != null)
+            {
+                Test.Log($"StartCutsceneImpl() WP_TeleportDestination = {__instance.WP_TeleportDestination.name} - {__instance.WP_TeleportDestination.position}");
+            }
+            else
+            {
+                Test.Log($"StartCutsceneImpl() WP_TeleportDestination = null");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(InteractableWorldMapPOI), "SetPOI", MethodType.Normal)]
+    class Patch_10
+    {
+        static void Postfix(ref POI point, ref InteractableWorldMapPOI __instance)
+        {
+            Test.Log($"SetPOI() point = {point.displayName}");
+        }
+    }
+
+    [HarmonyPatch(typeof(POI), "OnPOIOverlap", MethodType.Normal)]
+    class Patch_11
+    {
+        static void Postfix(bool didDiscover, ref POI __instance)
+        {
+            Test.Log($"OnPOIOverlap() point = {__instance.displayName}");
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldMapHUDController), "OnRadioClicked", MethodType.Normal)]
+    class Patch_12
+    {
+        static void Postfix(ref WorldMapHUDController __instance)
+        {
+            Test.Log($"OnRadioClicked()");
+        }
+    }
+
+    [HarmonyPatch(typeof(POI), "GetDisplayName", MethodType.Normal)]
+    class Patch_13
+    {
+        static void Postfix(ref string __result, ref POI __instance)
+        {
+            Test.Log($"GetDisplayName() __result = {__result}");
+        }
+    }
+
+    [HarmonyPatch(typeof(POI), "GetDisplayName", MethodType.Normal)]
+    class Patch_14
+    {
+        static void Postfix(ref string __result, ref POI __instance)
+        {
+            Test.Log($"GetDisplayName() __result = {__result}");
+        }
+    }
+
+    [HarmonyPatch(typeof(MiniMapWindow), "OnMarkerSelected", MethodType.Normal)]
+    class Patch_15
+    {
+        static void Postfix(ref MiniMapObject marker, ref MiniMapWindow __instance)
+        {
+            Test.Log($"OnMarkerSelected() text = {marker.Text}, LinkedObject = {marker.LinkedObject.name}");
+        }
+    }
+}
---
+++ WorldMapFastTravel/WorldMapFastTravel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+//using Harmony; // If you're using 0.3.0 and lower
+using HarmonyLib;
+using InXile.Trade;
+using InXile.UI.WL3;
+using UnityEngine;
+using InXile.Scenes;
+
+[assembly: MelonInfo(typeof(WorldMapFastTravel.WorldMapFastTravel), "World Map Fast Travel", "1.0.0", "Coomzy")]
+[assembly: MelonGame("InxileEntertainment", "Wasteland 3")]
+namespace WorldMapFastTravel
+{
+    public class WorldMapFastTravel : MelonMod
+    {
+        public static WorldMapFastTravel instance;
+
+        public static MelonPreferences_Category prefsCategory;
+        public static MelonPreferences_Entry<bool> canTravelToUndiscoveredEntry;
+
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+
+            instance = this;
+
+            //Log($"WorldMapFastTravel Init!");
+
+            prefsCategory = MelonPreferences.CreateCategory("World Map Fast Travel");
+            canTravelToUndiscoveredEntry = prefsCategory.CreateEntry("canTravelToUndiscovered", false);
+        }
+
+        void LogInternal(string log)
+        {
+            LoggerInstance.Msg(log);
+        }
+
+        public static void Log(string log)
+        {
+            instance.LogInternal(log);
+        }
+    }
+
+    [HarmonyPatch(typeof(MiniMapWindow), "OnMarkerClicked", MethodType.Normal)]
+    class Patch_MiniMapWindow_OnMarkerClicked
+    {
+        static void Postfix(ref MiniMapObject marker, ref MiniMapWindow __instance)
+        {
+            // Right mouse button allows fast travel
+            if (!Input.GetKeyUp(KeyCode.Mouse1))
+            {
+                return;
+            }
+
+            bool isDiscovered = true;
+
+            // Undiscovered locations will have the [Undiscovered] suffix, we need to remove it for the text to find the correct POI later
+            var textParsed = marker.Text;
+            int undiscoveredIndex = textParsed.IndexOf('[');
+            if (undiscoveredIndex != -1)
+            {
+                undiscoveredIndex--;
+                textParsed = textParsed.Substring(0, undiscoveredIndex);
+                isDiscovered = false;
+            }
+
+            // Setting for if we can fast travel to undiscovered locations, off by default as it's more likely to cause quest breaking bugs by missing triggers on the world map
+            if (!isDiscovered && !WorldMapFastTravel.canTravelToUndiscoveredEntry.Value)
+            {
+                return;
+            }
+            
+            //WorldMapFastTravel.Log($"OnMarkerClicked() text = {marker.Text}, textParsed = {textParsed}, LinkedObject = {marker.LinkedObject.name}");
+
+            var pois = UnityEngine.Object.FindObjectsOfType<POI>();
+            POI selectedPOI = null;
+            foreach (var poi in pois)
+            {
+                if (poi.displayName == textParsed)
+                {
+                    selectedPOI = poi;
+                    break;
+                }
+
+                //WorldMapFastTravel.Log($"POI displayName = {poi.displayName}");
+            }
+
+            var wmts = UnityEngine.Object.FindObjectsOfType<WorldMapTransition>();
+            WorldMapTransition selectedWmt = null;
+            foreach (var wmt in wmts)
+            {
+                if (selectedPOI != null && wmt.myPOI == selectedPOI)
+                {
+                    selectedWmt = wmt;
+                    break;
+                }
+
+                //WorldMapFastTravel.Log($"wmt POI displayName = {wmt.myPOI?.displayName}");
+            }
+                       
+            if (selectedWmt != null)
+            {
+                // Open Prompt for fast travel
+                WorldMapManager.instance.OpenPrompt(selectedWmt);
+
+                // Close the Mini Map Window
+                __instance.OnCloseClicked();
+
+                //WorldMapFastTravel.Log($"selectedPOI displayName = {selectedPOI.displayName}, uniqueId = {selectedPOI.uniqueId}");
+            }
+        }
+    }
+}
